fecccae0fc9455406ab06e25ede83dab205c00ee
Dhurkesh-B <dhurkeshmyself@gmail.com>
2025-06-12 12:12:28 +0530

    m1

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mcp",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "pydantic",
+        "groq",
+        "requests",
+        "python-dotenv",
+        "langchain-openai",
+        "python-multipart",
+        "markdown"
+    ],
+) 
